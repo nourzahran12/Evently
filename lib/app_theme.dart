a50274f360ef402b199e56ef9f01050ca6c75617
Nour Zahran <nourzahran1122@gmail.com>
2026-02-18 00:31:12 +0200

    Add bottom nav bar

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color primaryLightColor = Color(0xFF0E3A99);
+  static const Color primaryLight = Color(0xFF0E3A99);
   static const Color primaryDark = Color(0xFF457AED);
   static const Color backgroundLight = Color(0xFFF4F7FF);
   static const Color backgroundDark = Color(0xFF000F30);
@@ -9,7 +9,21 @@ class AppTheme {
   static const Color black = Color(0xFF1C1C1C);
   static const Color grey = Color(0xFF686868);
 
-  static ThemeData lightTheme = ThemeData();
+  static ThemeData lightTheme = ThemeData(
+    scaffoldBackgroundColor: backgroundLight,
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: white,
+      selectedItemColor: primaryDark,
+      unselectedItemColor: grey,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+    ),
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: primaryLight,
+      foregroundColor: white,
+      shape: CircleBorder(),
+    ),
+  );
 
   static ThemeData darkTheme = ThemeData();
 }
