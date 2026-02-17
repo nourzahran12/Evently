e1dbc8a1e4f6f93059b3c2f3ac57004bb4a453f5
Nour Zahran <nourzahran1122@gmail.com>
2026-02-17 23:28:50 +0200

    Add app theme

---
+++ lib/app_theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryLightColor = Color(0xFF0E3A99);
+  static const Color primaryDark = Color(0xFF457AED);
+  static const Color backgroundLight = Color(0xFFF4F7FF);
+  static const Color backgroundDark = Color(0xFF000F30);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF1C1C1C);
+  static const Color grey = Color(0xFF686868);
+
+  static ThemeData lightTheme = ThemeData();
+
+  static ThemeData darkTheme = ThemeData();
+}
---
+++ lib/homr_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomrScreen extends StatelessWidget {
+  static const String routName = '/HomeScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
