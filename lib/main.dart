d54eb820ba26e55756bc82e51c740a8b9066ce9e
Nour Zahran <nourzahran1122@gmail.com>
2026-02-21 01:00:34 +0200

    Integrate with Firebase on Android

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:evently/auth/login_screen.dart';
 import 'package:evently/auth/register_screen.dart';
 import 'package:evently/create_event_screen.dart';
 import 'package:evently/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(EventlyApp());
 }
 
